990b76f0e6e2a2a1e7fe5c7634258035cfa30eff
Rafael Gottardi (Fino) <rafael.gottardi@gmail.com>
2022-02-16 14:49:04 -0300

    Include prepare ubuntu server scripts

---
+++ currency-service/scripts/ubuntu-prepare.sh
@@ -0,0 +1,18 @@
+!#/bin/bash
+# Update system
+apt update -y && apt install python3 python3-pip git -y && apt upgrade python3 python3-pip -y
+
+# Change Work dir
+cd ~
+
+# Get repo
+git clone https://github.com/RafaelFino/simple-python-app.git
+
+# Change work dir
+cd simple-python-app/currency-service
+
+# Install app python dependencies
+pip3 install -r requirements.txt
+
+# Start application
+uvicorn app.main:app --reload --log-level trace --port 8081 --host 0.0.0.0 --log-config etc/log-config.yml
